345b703fcd509de553cdead75dc1f9bd75634d4a
Francisco Ramirez <francisco.ramirez3145@alumnos.udg.mx>
2025-10-21 16:04:05 -0600

    Creamos el archivo mostrar-lista.blade.php para dejar el index como pagina principal y usar este archivo para mostrar los alumnos registrados

---
+++ resources/views/alumnos/mostrar-lista.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Alumnos</title>
+</head>
+<body>
+    <h1>Lista de Alumnos</h1>
+    @if ($alumnos->count() > 0)
+    <table border="1">
+        <thead>
+        <tr>
+            <th>Codigo</th>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Fecha de Nacimiento</th>
+            <th>Sexo</th>
+            <th>Carrera</th>
+            <th>Editar</th>
+            <th>Eliminar</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($alumnos as $alumno)
+            <tr>
+                <td>{{ $alumno->codigo }}</td>
+                <td>{{ $alumno->nombre }}</td>
+                <td>{{ $alumno->correo }}</td>
+                <td>{{ $alumno->fecha_nacimiento }}</td>
+                <td>{{ $alumno->sexo }}</td>
+                <td>{{ $alumno->carrera }}</td>
+                <td>
+                    <form action="{{ route('alumnos.edit', $alumno->id) }}" method="POST">
+                        @csrf
+                        @method('GET')
+                        <button type="submit">Editar</button>
+                    </form>
+                </td>
+                <td>
+                    <form action="{{ route('alumnos.destroy', $alumno->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Eliminar</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>   
+    </table>
+ @else
+    <p>No hay alumnos registrados.</p>
+ @endif
+</body>
+</html>
