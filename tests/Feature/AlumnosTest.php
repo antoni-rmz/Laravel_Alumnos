70753c2de798c75f877d27f6514fb7be3f3b7c0b
Francisco Ramirez <francisco.ramirez3145@alumnos.udg.mx>
2025-10-22 01:02:02 -0600

    Agregamos las funciones para el test de nuestro CRUD de Alumnos

--- tests/Feature/AlumnosTest.php
+++ tests/Feature/AlumnosTest.php
@@ -10,31 +10,72 @@ use App\Models\Alumnos;
 class AlumnosTest extends TestCase
 {
     use RefreshDatabase;
-    /**
-     * Prueba de la pagina primcipal de alumnos para verificar que funciona correctamente.
-     * @return void
-     */
-    public function test_alumno_creation(): void
+    /** 
+    * Test para verificar que funcina el modelo, la migración, el controlador y los routes de Alumnos
+    */
+    public function test_alumnos()
     {
-        $response = $this->get('/alumnos');
-        $response->assertStatus(200);
-    }
-    /**
-     * Prueba que un alumno creado aparece en la lista.
-     *
-     * @return void
-     */
-    public function test_se_puede_ver_un_alumno_en_la_lista(): void
-    {
-        $alumno = Alumnos::factory()->create();
-        // Visitamos la página de alumnos
-        $response = $this->get('/alumnos');
-
-        // Comprobamos que la página cargó bien
-        $response->assertStatus(200);
+        //Usamos factory para crear un alumno
+        $datosAlumno = Alumnos::factory()->make();
         
-        // Y lo más importante: comprobamos que podemos ver
-        // el nombre de nuestro alumno de prueba en el HTML
-        $response->assertSee($alumno->nombre);
+        //guardamos el alumno usando la ruta correspondiente
+        $response = $this->post(route('alumnos.store'), $datosAlumno->toArray());
+
+        //Comprobamos que el alumno se ha guardado correctamente
+        $response->assertRedirect(route('alumnos.index'));
+        $this->assertDatabaseHas('alumnos', [
+            'codigo' => $datosAlumno['codigo'],
+            'nombre' => $datosAlumno['nombre'],
+            'correo' => $datosAlumno['correo'],
+            'fecha_nacimiento' => $datosAlumno['fecha_nacimiento'],
+            'sexo' => $datosAlumno['sexo'],
+            'carrera' => $datosAlumno['carrera'],
+        ]);
+        
+        //Obtenemos el alumno recien creado
+        $alumno = Alumnos::where('codigo', $datosAlumno['codigo'])->first();
+        $this->assertNotNull($alumno);
+
+        //Porbamos las rutas 
+        //Probamos la ruta de index
+        $response = $this->get(route('alumnos.index'));
+        $response->assertStatus(200);
+        $response->assertSee($datosAlumno['nombre']);
+
+        //Probamos la ruta de show
+        $response = $this->get(route('alumnos.show', ['alumno'=>$alumno->id]));
+        $response->assertStatus(200);
+        $response->assertSee($datosAlumno['nombre']);
+
+        //Probamos las rutas de edit y update
+        $response = $this->get(route('alumnos.edit', ['alumno'=>$alumno->id]));
+        $response->assertStatus(200);
+        $response->assertSee($datosAlumno['nombre']);
+
+        //Datos actualizados
+        $datosActualizados = Alumnos::factory()->make()->toArray();
+
+        //Actualizamos el alumno
+        $response = $this->put(route('alumnos.update', ['alumno'=>$alumno->id]), $datosActualizados);
+        $response->assertRedirect(route('alumnos.index'));
+
+        //Comprobamos que los datos se han actualizado
+        $this->assertDatabaseHas('alumnos', [
+            'codigo' => $datosActualizados['codigo'],
+            'nombre' => $datosActualizados['nombre'],
+            'correo' => $datosActualizados['correo'],
+            'fecha_nacimiento' => $datosActualizados['fecha_nacimiento'],
+            'sexo' => $datosActualizados['sexo'],
+            'carrera' => $datosActualizados['carrera'],
+        ]);
+
+        //Probamos la ruta de delete
+        $response = $this->delete(route('alumnos.destroy', ['alumno'=>$alumno->id]));
+        $response->assertRedirect(route('alumnos.index'));
+
+        //Comprobamos que el alumno se ha eliminado
+        $this->assertDatabaseMissing('alumnos', [
+            'id' => $alumno->id,
+        ]);
     }
 }
